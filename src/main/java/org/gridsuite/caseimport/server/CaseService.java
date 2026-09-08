4a0086e6392fde0c5e241d46e7d14837489b45a0
klesaulnier <42617371+klesaulnier@users.noreply.github.com>
2026-09-08 09:33:52 +0200

    Migrate rest template to rest client (#63)
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/main/java/org/gridsuite/caseimport/server/CaseService.java
+++ src/main/java/org/gridsuite/caseimport/server/CaseService.java
@@ -7,13 +7,11 @@
 package org.gridsuite.caseimport.server;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Objects;
@@ -29,13 +27,13 @@ public class CaseService {
     private static final String CASE_SERVER_API_VERSION = "v1";
 
     private static final String DELIMITER = "/";
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
     private String caseServerBaseUri;
 
     public CaseService(@Value("${powsybl.services.case-server.base-uri:http://case-server/}") String caseServerBaseUri,
-                       RestTemplateBuilder restTemplateBuilder) {
+                       RestClient.Builder restClientBuilder) {
         this.caseServerBaseUri = caseServerBaseUri;
-        this.restTemplate = restTemplateBuilder.build();
+        this.restClient = restClientBuilder.build();
     }
 
     public void setBaseUri(String caseServerBaseUri) {
@@ -53,11 +51,13 @@ public class CaseService {
                     .filename(Objects.requireNonNull(multipartFile.getOriginalFilename()));
         }
 
-        HttpEntity<MultiValueMap<String, HttpEntity<?>>> request = new HttpEntity<>(
-                multipartBodyBuilder.build(), headers);
         try {
-            caseUuid = restTemplate.postForObject(caseServerBaseUri + DELIMITER + CASE_SERVER_API_VERSION + "/cases", request,
-                    UUID.class);
+            caseUuid = restClient.post()
+                    .uri(caseServerBaseUri + DELIMITER + CASE_SERVER_API_VERSION + "/cases")
+                    .headers(httpHeaders -> httpHeaders.addAll(headers))
+                    .body(multipartBodyBuilder.build())
+                    .retrieve()
+                    .body(UUID.class);
         } catch (HttpStatusCodeException e) {
             if (e.getStatusCode().equals(HttpStatus.UNPROCESSABLE_ENTITY)) {
                 throw new CaseImportException(INCORRECT_CASE_FILE, e.getMessage());
